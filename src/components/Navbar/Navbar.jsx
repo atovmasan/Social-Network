b68f17d70a74e6054992bd41de93858b304154ac
atovmasan <yt988998@gmail.com>
2025-10-10 14:58:12 +0300

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import s from './Navbar.module.css';
-import {NavLink} from "react-router-dom";
+import React from "react";
+import s from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav className={s.nav}>
-            <div className={s.item}>
-                <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
-            </div>
-            <div className={`${s.item} ${s.active}`}>
-                <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
-            </div>
-            <div className={`${s.item} ${s.active}`}>
-                <NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className={s.nav}>
+      <div className={s.linkContainer}>
+        <div className={s.item}>
+          <NavLink to="/profile" activeClassName={s.activeLink}>
+            Profile
+          </NavLink>
+        </div>
+      </div>
+      <div className={s.linkContainer}>
+        <div className={`${s.item} ${s.active}`}>
+          <NavLink to="/dialogs" activeClassName={s.activeLink}>
+            Messages
+          </NavLink>
+        </div>
+      </div>
+      <div className={s.linkContainer}>
+        <div className={`${s.item} ${s.active}`}>
+          <NavLink to="/users" activeClassName={s.activeLink}>
+            Users
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -1,39 +1,50 @@
-import React from 'react';
+import React from "react";
 import styles from "./users.module.css";
 import userPhoto from "../../assets/images/user.png";
-import {NavLink} from "react-router-dom";
-import { Button } from 'antd';
+import { NavLink } from "react-router-dom";
+import { Button } from "antd";
 
-let User = ({user, followingInProgress, unfollow, follow}) => {
-    return (
-       <div className={styles.user}>
-                <span>
-                    <div>
-                       <NavLink to={'/profile/' + user.id}>
-                        <img src={user.photos.small != null ? user.photos.small : userPhoto}
-                             className={styles.userPhoto}/>
-                       </NavLink>
-                    </div>
-                    <div>
-                        {user.followed
-                            ? <button disabled={followingInProgress
-                                .some(id => id === user.id)}
-                                      onClick={() => { unfollow(user.id) }}>
-                                Unfollow</button>
-                            : <Button style={ { backgroundColor: '#5865f2'}} type='primary' disabled={followingInProgress.some(id => id === user.id)}
-                                      onClick={() => { follow(user.id) }}>
-                                      Follow</Button>}
-                                     
-                    </div>
-                </span>
-                <span>
-                    <span>
-                        <div>{user.name}</div>
-                        <div>{user.status}</div>
-                        <div></div>
-                    </span>
-                </span>
-            </div>)
-}
+let User = ({ user, followingInProgress, unfollow, follow }) => {
+  return (
+    <div className={styles.user}>
+      <span>
+        <div>
+          <NavLink to={"/profile/" + user.id}>
+            <img
+              src={user.photos.small != null ? user.photos.small : userPhoto}
+              className={styles.userPhoto}
+            />
+          </NavLink>
+        </div>
+        <div></div>
+      </span>
+      <span>
+        {user.followed ? (
+          <Button
+            disabled={followingInProgress.some((id) => id === user.id)}
+            onClick={() => {
+              unfollow(user.id);
+            }}
+          >
+            Unfollow
+          </Button>
+        ) : (
+          <Button
+            style={{ backgroundColor: "#5865f2" }}
+            type="primary"
+            disabled={followingInProgress.some((id) => id === user.id)}
+            onClick={() => {
+              follow(user.id);
+            }}
+          >
+            Follow
+          </Button>
+        )}
+        <div>{user.name}</div>
+        <div>{user.status}</div>
+      </span>
+    </div>
+  );
+};
 
 export default User;
